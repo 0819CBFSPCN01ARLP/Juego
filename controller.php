c6495c8373aa9493bbbc7f621e144895e5bd229c
Pablo <pgcorrales@hotmil.com>
2019-09-28 00:41:10 -0300

    updates

--- controller.php
+++ controller.php
@@ -29,4 +29,31 @@ function validarUsername() {
     
     }
 
+    function crearUsuario() {
+
+      $hash = password_hash($_POST["password"], PASSWORD_DEFAULT);
+      
+      $usuario = [
+      
+         "email" => $_POST["email"],
+      
+         "username" => $_POST["username"],
+      
+         "password" => $hash,
+      
+       ];
+      
+       $usuarios = file_get_contents("usuarios.json");
+      
+       $usuariosArray = json_decode($usuarios, true);
+      
+       $usuariosArray [] = $usuario;
+      
+       $usuariosFinal = json_encode($usuariosArray);
+      
+       file_put_contents("usuarios.json", $usuariosFinal);
+      
+      }
+
 ?>
+
