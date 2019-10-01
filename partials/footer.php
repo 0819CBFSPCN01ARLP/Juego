aed60a31428c79f31ef05bb91678ac1c6efd10b1
Pablo <pgcorrales@hotmil.com>
2019-10-01 13:44:48 -0300

    .

---
+++ partials/footer.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+  <div class="container justify-center float-left">
+    <ul>
+      <li class="mr-auto"><a href="https://facebook.com" target="_blank">Facebook</a></li>
+      <li><a href="https://instagram.com" target="_blank">Instagram</a></li>
+      <li><a href="https://twitter.com" target="_blank">Twitter</a></li>
+    </ul>  
+  </div>
+</footer>
