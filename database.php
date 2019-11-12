b2178e52a2020ad5bd5f226f36dae9d1d6ca6045
Pablo <pgcorrales@hotmil.com>
2019-11-11 23:36:15 -0300

    .

--- database.php
+++ database.php
@@ -15,17 +15,3 @@ try {
   die('Connection Failed: ' . $e->getMessage());
 }
 ?>
-
-<?php
-$server= "localhost";
-$username= "root";
-$password= "";
-$database= "Juego";
-
-try{
-  $conn= new PDO("mysql:host=$server;dbname=$database;",$username,$password);
-} catch(PDOExeption $e){
-  die("Connected failed:".$e->getMessage());
-}
-
-?>
