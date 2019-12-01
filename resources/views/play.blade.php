98a544cc16cdfc08a45c506e4fb11b0120be5f88
Pablo <pgcorrales@hotmil.com>
2019-12-01 15:22:06 -0300

    .

--- resources/views/play.blade.php
+++ resources/views/play.blade.php
@@ -4,13 +4,109 @@
 
 @section('head')
 <link rel="stylesheet" href="/css/jugar.css">
+<script>
+
+	var inicio=0;
+
+	var timeout=0;
+
+ 
+
+	function empezarDetener(elemento)
+
+	{
+
+		if(timeout==0)
+
+		{
+
+			// empezar el cronometro
+
+ 
+
+			elemento.value="Detener";
+
+ 
+
+			// Obtenemos el valor actual
+
+			inicio=vuelta=new Date().getTime();
+
+ 
+
+			// iniciamos el proceso
+
+			funcionando();
+
+		}else{
+
+			// detemer el cronometro
+
+ 
+
+			elemento.value="Empezar";
+
+			clearTimeout(timeout);
+
+			timeout=0;
+
+		}
+
+	}
+
+ 
+
+	function funcionando()
+
+	{
+
+		// obteneos la fecha actual
+
+		var actual = new Date().getTime();
+
+ 
+
+		// obtenemos la diferencia entre la fecha actual y la de inicio
+
+		var diff=new Date(actual-inicio);
+
+ 
+
+		// mostramos la diferencia entre la fecha actual y la inicial
+
+		var result=LeadingZero(diff.getUTCHours())+":"+LeadingZero(diff.getUTCMinutes())+":"+LeadingZero(diff.getUTCSeconds());
+
+		document.getElementById('crono').innerHTML = result;
+
+ 
+
+		// Indicamos que se ejecute esta función nuevamente dentro de 1 segundo
+
+		timeout=setTimeout("funcionando()",1000);
+
+	}
+
+ 
+
+	/* Funcion que pone un 0 delante de un valor si es necesario */
+
+	function LeadingZero(Time) {
+
+		return (Time < 10) ? "0" + Time : + Time;
+
+	}
+
+	</script>
 @endsection
 
 @section('content')
+
   <div class="container pt-5">
     <div class="row">
       <div class="col-6"><h2 class="float-left">Puntaje</h2></div>
-      <div class="col-6"><h2 class="float-right">Tiempo</h2></div>
+      <div class="col-6"><h2 class="float-right">Tiempo</h2></div>      
+      <div class="crono_wrapper col-12"><h2 id="crono" class="float-right">00:00:00</h2></div>
+      <input type="button" value="Empezar" onclick=empezarDetener(this);>
     </div>
   </div>
   <!-- preguntas  -->
